8a8837032ef2245ca5bf964167a41bc2c5508df0
AliSamiee <alisamiee844@gmail.com>
2024-06-25 19:55:51 +0330

    Add models and data to database

--- Tasks/admin.py
+++ Tasks/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserAccount
+from .models import UserAccount, Tasks, Category, Comment
 
 # Register your models here.
 class UserAccountAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(UserAccount, UserAccountAdmin)
+admin.site.register([Tasks, Category, Comment])
--- Tasks/models.py
+++ Tasks/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.core.exceptions import ValidationError
 
+
+def validate_precentage(value):
+        if value < 0 or value > 100 :
+            raise ValidationError('percentage must be between 0 and 100')
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, name , password=None):
@@ -57,4 +62,31 @@ class UserAccount(AbstractBaseUser):
 
 
 
+class Tasks(models.Model):
+    name = models.CharField(max_length=50)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    progress = models.DecimalField(max_digits=5, decimal_places=2, validators=[validate_precentage])
+    functor = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+    def __str__(self) -> str:
+        return self.name
     
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Comment(models.Model):
+    Author = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+        
+    def __str__(self):
+        return f'{self.Author} comment for {self.task}'
