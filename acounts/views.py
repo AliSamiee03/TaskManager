556bf19e36639d23f3c05ea6d8874a76c5f7b317
AliSamiee <alisamiee844@gmail.com>
2024-07-17 15:19:57 +0330

    The login and logout pages have been completed

--- acounts/views.py
+++ acounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email= email, password= password)
+        if user is not None :
+            login(request, user)
+            messages.info(request, "success", extra_tags='success')
+            return redirect('Home')
+        else:
+            messages.info(request, "error", extra_tags='error')
+            return redirect('login')
+    
+    else :
+        return render(request, 'accounts/login.html')
+    
+def logout_view(request):
+
+    logout(request)
+    messages.info(request, 'logout', extra_tags='logout')
+    return redirect('Home')
