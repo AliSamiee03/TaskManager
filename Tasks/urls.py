1115ed94c8bb74eb6f6483b2b0d3fdb4810dc4e0
AliSamiee <alisamiee844@gmail.com>
2024-07-19 13:17:35 +0330

    Complete the update section of the tasks

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_task_view, show_all_tasks_view, delete_task_view
+from .views import create_task_view, show_all_tasks_view, delete_task_view, update_task_view
 
 urlpatterns = [
     path('create-task/', create_task_view, name='create-task'),
     path('show-all-tasks/', show_all_tasks_view, name='show-tasks'),
     path('delete/<task_id>/', delete_task_view, name='delete-task'),
+    path('update/<task_id>/', update_task_view, name='update-task'),
 ]
--- Tasks/views.py
+++ Tasks/views.py
@@ -37,8 +37,33 @@ def show_all_tasks_view(request):
     return render(request, 'Tasks/show-all-tasks.html', context)
 
 def delete_task_view(request, task_id):
-    print(task_id)
     task = Tasks.objects.get(id=task_id)
     task.delete()
     messages.success(request, 'Task Deleted !', extra_tags='delete')
     return redirect('show-tasks')
+
+def update_task_view(request, task_id):
+    task = Tasks.objects.get(id=task_id)
+    if request.method == 'POST':
+        form = CreateTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            if task.start_date > task.end_date :
+                messages.error(request, 'The start time must be before the end time', extra_tags='time-input-error')
+                return redirect('update-task', task_id=task_id)
+
+            if 0 > task.progress or task.progress > 100 :
+                messages.error(request, 'Progress should be between 0 and 100', extra_tags='progress-error')
+                return redirect('update-task', task_id=task_id)   
+            
+            task.functor = request.user
+            messages.success(request, 'The task update was successful', extra_tags='update-task')
+            task.save()
+            return redirect('show-tasks')
+    else:
+
+        form = CreateTaskForm(instance=task)
+    context = {
+        'form' : form,
+    }
+    return render(request, 'Tasks/create_task.html', context)
