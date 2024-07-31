dcf0ece8ff5d855c0b08f4df7c58569ac2715a62
AliSamiee <alisamiee844@gmail.com>
2024-07-31 21:36:27 +0330

    update urls.py

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import create_task_view, show_all_tasks_view, delete_task_view, update_task_view, show_categories, create_category_view, delete_category_view, update_category_view, show_task_by_category_view, comments_view, delete_comment_view, show_public_tasks_view, search_view
+from . import views
 
 urlpatterns = [
-    path('create-task/', create_task_view, name='create-task'),
-    path('show-all-tasks/', show_all_tasks_view, name='show-tasks'),
-    path('delete/<task_id>/', delete_task_view, name='delete-task'),
-    path('update/<task_id>/', update_task_view, name='update-task'),
-    path('show-categories/', show_categories, name='categories'),
-    path('create-category/', create_category_view, name='create-category'),
-    path('delete-category/<category_id>/', delete_category_view, name='delete-category'),
-    path('update-category/<category_id>/', update_category_view, name='update-category'),
-    path('show-tasks-by-category/<category_id>/', show_task_by_category_view, name='task-by-category'),
-    path('comments/<task_id>/', comments_view, name='comments'),
-    path('delete-comment/<task_id>/<comment_id>/', delete_comment_view, name='delete-comment'),
-    path('public-tasks/', show_public_tasks_view, name='public-tasks'),
-    path('search/', search_view, name='search')
+    path('create-task/', views.create_task_view, name='create-task'),
+    path('show-all-tasks/', views.show_all_tasks_view, name='show-tasks'),
+    path('delete/<task_id>/', views.delete_task_view, name='delete-task'),
+    path('update/<task_id>/', views.update_task_view, name='update-task'),
+    path('show-categories/', views.show_categories, name='categories'),
+    path('create-category/', views.create_category_view, name='create-category'),
+    path('delete-category/<category_id>/', views.delete_category_view, name='delete-category'),
+    path('update-category/<category_id>/', views.update_category_view, name='update-category'),
+    path('show-tasks-by-category/<category_id>/', views.show_task_by_category_view, name='task-by-category'),
+    path('comments/<task_id>/', views.comments_view, name='comments'),
+    path('delete-comment/<task_id>/<comment_id>/', views.delete_comment_view, name='delete-comment'),
+    path('public-tasks/', views.show_public_tasks_view, name='public-tasks'),
+    path('search/', views.search_view, name='search')
 ]
